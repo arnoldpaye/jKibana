78f25df22646b22254ddd29675cc0267a1ea3302
Arnold Paye <arnoldpayet@gmail.com>
2016-08-18 11:26:18 -0400

    add Search button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var queryString = 'application:"ST7" AND environment:"qa1" AND type:"jsexception"';
-var today = new Date();
 
-jKibana.search(queryString, today).then(function(response) {
-    console.log(response, $);
-}, function (error) {
-    console.log(error);
+$("#btnSearch").click(function(e) {
+    var today = new Date();
+    jKibana.search(queryString, today).then(function(response) {
+        console.log(response, $);
+    }, function (error) {
+        console.log(error);
+    });
 });
