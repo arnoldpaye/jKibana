8ef58ec2889bf2f135e42c37fa10b546c06f7897
Arnold Paye <arnoldpayet@gmail.com>
2016-08-18 16:07:38 -0400

    add exceptions table

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var queryString = 'application:"ST7" AND environment:"qa1" AND type:"jsexception"';
 
 $("#btnSearch").click(function(e) {
+    $("#content").empty();
     var today = new Date();
     jKibana.search(queryString, today).then(function(response) {
-        console.log(response, $);
+        $("#content").append('<table>');
+        $("#content").append('<tr><th>ClassName</th><th>Message</th></tr>');
+        var exceptions = response.hits.hits;
+        console.log("Exceptions", exceptions.length);
+        for (var i = 0; i < exceptions.length; i++) {
+            var error = JSON.parse(exceptions[i]["_source"]["message"])["Details"]["Error"];
+            $("#content").append('<tr><td>' + error.ClassName + '</td><td>' + error.Message + '</td></tr>');
+        }
+        $("#content").append('</table>');
     }, function (error) {
         console.log(error);
     });
